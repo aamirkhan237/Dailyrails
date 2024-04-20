80adb3244187bb50764021afc9b9bbb090a40bc3
aamirkhan237 <aamir.4u71@gmail.com>
2024-04-20 16:32:44 +0530

    Facebooklogin

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -26,6 +26,7 @@ Devise.setup do |config|
   # with default "from" parameter.
   config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
   config.omniauth :google_oauth2, ENV['GOOGLE_OAUTH_CLIENT_ID'], ENV['GOOGLE_OAUTH_CLIENT_SECRET']
+  config.omniauth :facebook, Rails.application.credentials.facebook[:APP_ID], Rails.application.credentials.facebook[:APP_SECRET], token_params: { parse: :json }
   # config.omniauth :facebook, ENV[«HEROKU_OAUTH_ID»], ENV[ «HEROKU_OAUTH_SECRET»],
 
   # Configure the class responsible to send e-mails.
