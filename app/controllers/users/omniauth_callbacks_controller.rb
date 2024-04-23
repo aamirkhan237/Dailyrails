3bce0f7b1b111ca1ae54eebe5b4228743d9c1781
aamirkhan237 <aamir.4u71@gmail.com>
2024-04-23 20:10:25 +0530

    ActiveAdmin phase 1

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -7,51 +7,27 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   # You should also create an action method in this controller like this:
   # def twitter
   # end
-
-  def google_oauth2
-    user = User.from_omniauth(auth)
+  def omniauth(provider)
+    user = User.from_omniauth(request.env["omniauth.auth"])
     if user.present?
       sign_out_all_scopes
-      flash[:success] = t 'devise.omniauth_callbacks.success', kind: 'Google'
+      flash[:success] = t 'devise.omniauth_callbacks.success', kind: provider.capitalize
       sign_in_and_redirect user, event: :authentication
     else
-      flash[:alert] =
-        t 'devise.omniauth_callbacks.failure', kind: 'Google', reason: "#{auth.info.email} is not authorized."
+      flash[:alert] = t 'devise.omniauth_callbacks.failure', kind: provider.capitalize, reason: "#{auth.info.email} is not authorized."
       redirect_to new_user_session_path
     end
   end
-  def facebook
-    @user = User.from_omniauth(request.env["omniauth.auth"])
-    if @user.persisted?
-      sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
-      set_flash_message(:notice, :success, kind: "Facebook") if is_navigational_format?
-    else
-      session["devise.facebook_data"] = request.env["omniauth.auth"].except(:extra) # Removing extra as it can overflow some session stores
-      redirect_to new_user_registration_url
-    end
+  
+  def google_oauth2
+    omniauth(:google_oauth2)
   end
-  # More info at:
-  # https://github.com/heartcombo/devise#omniauth
+  
+  def facebook
+    omniauth(:facebook)
+  end
 
-  # GET|POST /resource/auth/twitter
-  # def passthru
-  #   super
-  # end
-
-
-  # GET|POST /users/auth/twitter/callback
-  # def failure
-  #   super
-  # end
-
-
-
-  # protected
-
-  # The path used when OmniAuth fails
-  # def after_omniauth_failure_path_for(scope)
-  #   super(scope)
-  # end
+ 
   private
   def auth
     @auth ||=request.env['omniauth.auth']
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,17 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :omniauthable,
          omniauth_providers: [:google_oauth2,:facebook]
-         validates :first_name, :last_name, :mobile_number, :location, :date_of_birth, presence: true
+         validates :first_name, :last_name, :mobile_number, :location, :date_of_birth, presence: true, unless: :omniauth_sign_in?
+
 
          def self.from_omniauth(auth)
           name_split = auth.info.name.split(" ")
           user = User.where(email: auth.info.email).first
           user ||= User.create!(provider: auth.provider, uid: auth.uid, last_name: name_split[0], first_name: name_split[1], email: auth.info.email, password: Devise.friendly_token[0, 20])
             user
+        end
+        def omniauth_sign_in?
+          provider.present? # Check if the user is signing in via OmniAuth
         end
         
 end 
