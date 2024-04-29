f90f70a1271295e6713a5232675fe9c0b405e595
aamirkhan237 <aamir.4u71@gmail.com>
2024-04-30 00:30:28 +0530

    Images#show

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -45,7 +45,7 @@ class ProductsController < ApplicationController
     private
   
     def product_params
-      params.require(:product).permit(:name, :description, :price)
+      params.require(:product).permit(:name, :description, :price, :image)
     end
   end
   
