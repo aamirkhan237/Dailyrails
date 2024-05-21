2c803896230a275f567abccca01a1fca2b381df6
aamirkhan237 <aamir.4u71@gmail.com>
2024-05-21 21:12:41 +0530

    design change in new.html for order and order placed

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,12 +1,11 @@
 class OrdersController < ApplicationController
     before_action :authenticate_user!
+    protect_from_forgery with: :exception, unless: -> { request.format.json? }
   
     def new
       @order = Order.new
       @product = Product.find(params[:product_id]) if params[:product_id].present?
-
-      puts "@product assigned:", @product
-      
+  
       if @product.present?
         @order.order_items.build(product: @product, quantity: 1)
       else
@@ -15,27 +14,47 @@ class OrdersController < ApplicationController
           @order.order_items.build(product: cart_item.product, quantity: cart_item.quantity)
         end
       end
-
+  
       @addresses = current_user.addresses
       @address = Address.new 
     end
   
-    # def create
-    #   @order = Order.new(order_params)
-    #   @order.user = current_user
-    #   @order.address = current_user.addresses.find(params[:order][:address_id])
+    def create
+        Rails.logger.debug "OrdersController#create called"
+        @order = current_user.orders.build(order_params)
 
-    #   if @order.save
-    #     flash[:notice] = "Your order has been placed successfully!"
-    #     redirect_to orders_path
-    #   else
-    #     render 'new'
-    #   end
-    # end
+        if params[:product_id].present?
+            product = Product.find(params[:product_id])
+            @order.order_items.build(product: product, quantity: 1)
+          else
+            @cart = current_user.cart
+            @cart.cart_items.each do |cart_item| 
+              @order.order_items.build(product: cart_item.product, quantity: cart_item.quantity)
+            end
+          end
+        @order.total_price = @order.order_items.sum { |item| item.product.price * item.quantity }
 
+            if @order.save
+            Rails.logger.debug "Order saved successfully"
+            @cart.cart_items.destroy_all if @cart.present? # Clear the cart after the order is placed
+            redirect_to confirmation_order_path(@order)
+            else
+            Rails.logger.debug "Order save failed: #{@order.errors.full_messages}"
+            @addresses = current_user.addresses
+            render :new
+            end
+    end
+  
+    def confirmation
+        @order = Order.find(params[:id])
+      end
+      
     private
   
     def order_params
-      params.require(:order).permit(:address_id, order_items_attributes: [:product_id, :quantity])
+      params.require(:order).permit(:address_id,:total_price, order_items_attributes: [:product_id, :quantity])
     end
+    # def address_params
+    #     params.require(:order).permit(:name, :street, :city, :state, :zip)
+    #   end
   end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,13 +1,15 @@
 class Order < ApplicationRecord
-  before_save :calculate_total_price
+  # before_validation :set_total_price
   validates :total_price, presence: true
   belongs_to :user
   has_many :order_items, dependent: :destroy
   belongs_to :address
   enum state: { pending: 0, paid: 1, shipped: 2, cancelled: 3 }
+  # accepts_nested_attributes_for :order_items
 
   private
-  def calculate_total_price
-    self.total_price = order_items.sum { |item| item.product.price * item.quantity }
-  end
+  # def set_total_price
+  #   self.total_price ||= order_items.sum { |item| item.product.price * item.quantity }
+  # end
+ 
 end
